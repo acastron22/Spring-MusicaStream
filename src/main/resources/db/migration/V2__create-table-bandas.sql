73363c54a22e9a72c5cc1a05001ff3784be9d9ad
Ariel <arielcastron22@hotmail.com>
2024-04-20 22:50:59 -0300

    criação dos migrations do código

---
+++ src/main/resources/db/migration/V2__create-table-bandas.sql
@@ -0,0 +1,7 @@
+create TABLE bandas(
+                       id bigint not null auto_increment,
+                       nome varchar(100) not null,
+                       resumo varchar(256) not null,
+
+                       primary key (id)
+);
